d0e912fcc6e3b8435a7d9fac29c86bd284af302f
Nawaraj-kandel <kandeln309@gmail.com>
2026-08-04 09:57:26 +0545

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,3 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import path from 'path'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   resolve: {
-//     alias: {
-//       '@': path.resolve(__dirname, './src'),
-//     },
-//   },
-// })
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath } from 'url'
@@ -21,9 +6,11 @@ import { dirname, resolve } from 'path'
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio/',
+
   plugins: [react()],
+
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),
